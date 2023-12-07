19f9754dbaf2fcb0374eeab283f8518f66ca52c2
Suami Rocha <suamirochati@gmail.com>
2023-12-06 22:09:47 -0300

    att

--- src/routes/components/core/Navigation.jsx
+++ src/routes/components/core/Navigation.jsx
@@ -2,7 +2,7 @@
 const Navbar = () => {
 
   return (
-    <header className={`flex w-full justify-evenly bg-geen-500 `}>
+    <header className={`flex w-full justify-evenly bg-geen-500 p-2`}>
       <div className="container">
         <div className="relative m-4 flex items-center justify-between">
           <div className="max-w-full px-4">
@@ -10,11 +10,11 @@ const Navbar = () => {
               <img
                 src="./logo-icon.svg"
                 alt="logo"
-                className="w-[100px] h-[100px]"
+                className="w-[50px] h-[50px]"
               />
             </a>
           </div>
-          <div className="flex pr- gap-1 sm:flex lg:pr-0">
+          <div className="flex pr- gap-2 sm:flex lg:pr-0">
              <a
                href="/#"
                className="px-2 py-3 text-base font-bold text-black rounded-md border border-solid border-black  hover:text-primary dark:text-black"
